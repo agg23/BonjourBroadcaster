219dd0bd56cc28c0878526b37c8cc3f2d7054b8b
Adam Gastineau <adam@appcannonsoftware.com>
2015-10-01 21:45:04 -0400

    Added basic Menu Bar support

--- Bonjour Broadcaster/AppDelegate.m
+++ Bonjour Broadcaster/AppDelegate.m
@@ -18,10 +18,31 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.statusBarItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [self.statusBarItem setTitle:@"B"];
+    [self.statusBarItem setHighlightMode:YES];
+    
+    NSMenu *menu = [[NSMenu alloc] init];
+    [menu addItemWithTitle:@"Show Bonjour Browser" action:@selector(showBrowser:) keyEquivalent:@""];
+    [menu addItem:[NSMenuItem separatorItem]];
+    [menu addItemWithTitle:@"Quit Bonjour Browser" action:@selector(quit:) keyEquivalent:@""];
+    
+    [self.statusBarItem setMenu:menu];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void)showBrowser:(id)sender
+{
+    [self.window makeKeyAndOrderFront:self];
+    [NSApp activateIgnoringOtherApps:YES];
+}
+
+- (void)quit:(id)sender
+{
+    [[NSApplication sharedApplication] terminate:nil];
 }
 
 @end
